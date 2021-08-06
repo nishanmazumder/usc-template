6e9a4f1c04a43381081432045121f37872f7207e
nishanmazumder <arosh019@gmail.com>
2021-08-06 23:34:39 +0600

    footer menu done

--- classes/class-sidebar.php
+++ classes/class-sidebar.php
@@ -15,26 +15,9 @@ class Sidebar
 
     use Singleton;
 
-    public $nm_widget_id;
-    //public $nm_widget_name = "from template";
-    public $nm_widget_name;
-    public $nm_widget_before;
-    public $nm_widget_after;
-    public $nm_widget_before_title;
-    public $nm_widget_after_title;
-
     protected function __construct()
     {
         $this->setup_hooks();
-
-        //$this->display_widget($this->nm_widget_name);
-
-        //$this->widget_init_hook();
-
-
-
-        $this->wid_reg();
-        
 
     }
 
@@ -42,100 +25,7 @@ class Sidebar
     {
         //Actions
         add_action( 'widgets_init', [$this, 'register_sidbars'] );
-
-        // Register new widget - Name Submission
-        add_action( 'widgets_init', [$this, 'nm_name_widget'] );
     }
-
-    public function wid_reg(){
-        if(!empty($this->nm_widget_name)){
-            add_action( 'widgets_init', [$this, 'test_widget'] );
-        }
-    }
-
-
-    //Have to do same with that name block
-    public function get_info($name = null){
-        if($name === null){
-            $this->nm_widget_name;
-        }else{
-            $this->nm_widget_name = $name;
-        }
-    }
-
-
-    public function test_widget(){
-
-        // if(isset($name)){
-        //     $name = $this->get_info($name);
-        // }else{
-        //     $name = "No Name Widget";
-        // }
-
-        
-
-        //$name = $this->get_info();
-
-         //Sidebar
-        register_sidebar(
-            array(
-                'id'            => 'test-widget',
-                'name'          => __( $this->nm_widget_name, 'nm_theme'),
-                'description'   => __( 'Widgets for page sidebar', 'nm_theme'),
-                'before_widget' => '<div id="%1$s" class="col-md-3 col-sm-6 col-xs-12 %2$s">',
-                'after_widget'  => '</div>',
-                'before_title'  => '<h4 class="nm_font_mont">',
-                'after_title'   => '</h4>',
-            )
-        );
-    }
-
-
-
-    // public function display_widget($name){
-
-    //     //$this->get_info($name);
-
-    //     //dynamic_sidebar($id);
-
-
-        
-    //     //return $name;
-    // }
-
-
-    public function widget_init_hook(){
-        $this->wid_reg();
-    }
-
-
-
-
-    public $myname;
-
-    public function set_name($name = null){
-
-        if($name === null){
-            return $this->myname;
-        }else{
-            return $this->myname = $name;
-        }
-
-      
-    }
-
-    public function echo_name(){
-        return $name = $this->get_name();
-    }
-
-    public function get_name(){
-        return $this->myname;
-    }
-
-
-
-
-
 
     public function register_sidbars(){
         //Post Sidebar
@@ -183,19 +73,12 @@ class Sidebar
                 'id'            => 'footer-sidebar',
                 'name'          => __( 'Footer Menu', 'nm_theme'),
                 'description'   => __( 'Widgets for footer social', 'nm_theme'),
-                'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                'before_widget' => '<div id="%1$s" class="col-md-3 col-sm-6 col-xs-12 %2$s">',
                 'after_widget'  => '</div>',
-                'before_title'  => '<h3 class="widget-title">',
-                'after_title'   => '</h3>',
+                'before_title'  => '<h4 class="nm_font_mont">',
+                'after_title'   => '</h4>',
             )
         );
     }
-
-
-    // Custom Widget register
-    public function nm_name_widget(){
-        register_widget( 'NM_THEME\Inc\Classes\Name_Submission' );
-    }
-
-    
+ 
 }
--- template-parts/footer/site-widgets.php
+++ template-parts/footer/site-widgets.php
@@ -7,82 +7,11 @@
 
 ?>
 
-<?php
-
-$widget = \NM_THEME\Inc\Classes\Sidebar::get_instance();
-
-
-?>
-
 <div class="row nm_widget">
-    <div class="col-md-3 col-sm-6 col-xs-12">
-        <?php
-        $widget->get_info('From Template');
-        //$widget->display_widget();
-
-        echo $widget->nm_widget_name;
-
-        //$widget->set_name("Nishan");
-        //$widget->echo_name();
-
-
-        
-
-        
-
-        ?>
-        <!-- <h4 class="nm_font_mont">ENGINEERING</h4>
-        <ul>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-        </ul> -->
-    </div>
+    <?php
+    if (is_active_sidebar('footer-sidebar')) {
+        dynamic_sidebar('footer-sidebar');
+    }
+    ?>
 </div>
 </div>
-
-<!-- <div class="row nm_widget">
-    <div class="col-md-3 col-sm-6 col-xs-12">
-        <h4 class="nm_font_mont">ENGINEERING</h4>
-        <ul>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-        </ul>
-    </div>
-    <div class="col-md-3 col-sm-6 col-xs-12">
-        <h4 class="nm_font_mont">ENGINEERING</h4>
-        <ul>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-        </ul>
-    </div>
-    <div class="col-md-3 col-sm-6 col-xs-12">
-        <h4 class="nm_font_mont">ENGINEERING</h4>
-        <ul>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-            <li><a href="#">Important Link</a></li>
-        </ul>
-    </div>
-    <div class="col-md-3 col-sm-6 col-xs-12">
-        <h4 class="nm_font_mont">ENGINEERING</h4>
-        <ul>
-            <li>Email</li>
-            <li>Email</li>
-            <li>Email</li>
-            <li>Email</li>
-            <li>Email</li>
-        </ul>
-    </div>
-</div>
-</div> -->
